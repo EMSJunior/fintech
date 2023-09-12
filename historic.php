10d6c5f01ab3b36703b761d2604445f9e6fc5d50
wanderson.silva <wanderson.silva@jrsistemas.net>
2023-09-12 18:50:09 -0300

    Project completion

--- historic.php
+++ historic.php
@@ -1,97 +1,99 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <script src="https://kit.fontawesome.com/36e17004f7.js" crossorigin="anonymous"></script>
-    <title>Historic</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style.css">
+  <title>Historic</title>
 </head>
 
 <body>
-    <header>
-        <h2>Web development</h2>
-    </header>
+  <header>
+    <h2>Web development</h2>
+  </header>
 
-    <main>
-          
-        <form action="historic.php" method="get">
-          <fieldset>
-            <legend>Search</legend>
-            ID: <input type="number" name="id" id="id" step="1" <?php if (isset($_GET['id'])) echo 'value="'.  $_GET['id'] . '"';?>>
-            <input type="submit" value="Search">
-          </fieldset>
-        </form>
+  <main>
+    <form action="historic.php" method="get">
+      <fieldset>
+        <legend>Search</legend>
 
-        <?php
-          require_once './classes/autoloader.class.php';
+        <label for="id">ID: </label>
+        <input type="number" name="id" id="id" step="1" <?php if (isset($_GET['id'])) echo 'value="' .  $_GET['id'] . '"'; ?>>
 
-          if(isset($_GET["id"]) && $_GET["id"] != ""){
+        <input type="submit" value="Search">
+      </fieldset>
+    </form>
 
-            R::setup('mysql:host=localhost;dbname=fintech', 'root', '');
-            $simulation = R::load('simulation', $_GET["id"]);
+    <?php
+    require_once './classes/autoloader.class.php';
 
-            if ($simulation->id == 0){
-              echo "Id não encontrado!";
-            }
-            else
-            {
+    if (isset($_GET["id"]) && $_GET["id"] != "") 
+    {
 
-              echo "<ul>";
+      R::setup('mysql:host=localhost;dbname=fintech', 'root', '');
+      $simulation = R::load('simulation', $_GET["id"]);
 
-              echo "<li>ID: {$simulation->id}</li>";
-              echo "<li>Cliente: {$simulation->client}</li>";
-              echo "<li>Contribuição Inicial R$ ". number_format($simulation->initialContribution, 2, ',' , '.') . "</li>";
-              echo "<li>Período: {$simulation->period} meses</li>";
-              echo "<li>Taxa mensal: {$simulation->monthlyIncome} %</li>";
-              echo "<li>Contribuição mensal: " . number_format($simulation->monthlyContribution, 2, ',' , '.') . "</li>";
+      if ($simulation->id == 0) 
+      {
+        echo "<p>Id not found!</p>";
+      } 
+      else 
+      {
+        echo "<ul>";
+        echo"<h3>Data:</h3>";
+        echo "<li>ID: {$simulation->id}</li>";
+        echo "<li>Client: {$simulation->client}</li>";
+        echo "<li>Initial contribution (R$): " . number_format($simulation->initialContribution, 2, ',', '.') . "</li>";
+        echo "<li>Period: {$simulation->period} meses</li>";
+        echo "<li>Monthly income (%): {$simulation->monthlyIncome} %</li>";
+        echo "<li>Monthly contribution: " . number_format($simulation->monthlyContribution, 2, ',', '.') . "</li>";
 
-              echo "</ul>";
+        echo "</ul>";
 
-              echo "<fieldset>
-                        <legend>Resultado</legend>
-                        <table>
-                          <thead>
-                              <tr>
-                                  <th>Mês</th>
-                                  <th>Valor Inicial (R$)</th>
-                                  <th>Aporte (R$)</th>
-                                  <th>Rendimento (R$)</th>
-                                  <th>Total (R$)</th>
-                              </tr>
-                          </thead>
-                          <tbody>";         
-                              for ($i = 1; $i <= $simulation->period; $i++) {
-                                  echo "<tr>";
-                                  echo "<td>" . $i . "</td>";
-                                  echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
-                                  echo "<td>" . ($i > 1 ? number_format($simulation->monthlyContribution, 2, ",", ".") : "---") . "</td>";
-                                  echo "<td>" . number_format($simulation->initialContribution * $simulation->monthlyIncome / 100, 2, ",", ".") . "</td>";
-                                  $simulation->initialContribution = $simulation->initialContribution * (1 + ($simulation->monthlyIncome / 100));
-                                  echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
-                                  $simulation->initialContribution += $simulation->monthlyContribution;
-                                  echo "</tr>";
-                              }
-                echo "    </tbody>
-                        </table>
-                    </fieldset>";
-              }
-            }
-            R::close();
-                             
-        ?>
-      
-        </fieldset>
-          
-        <br><br>
-        <a href="index.html"><button>Back</button></a>
-    </main>
-    
-    <footer>
-        <p>WanRod e Edson Júnior- &copy; 2023</p>
-    </footer>
+        echo "<table>
+                <thead>
+                  <tr>
+                    <th>Month</th>
+                    <th>Initial value (R$)</th>
+                    <th>Contribution (R$)</th>
+                    <th>Income (R$)</th>
+                    <th>Total (R$)</th>
+                  </tr>
+                </thead>
+
+                <tbody>";
+        for ($i = 1; $i <= $simulation->period; $i++) 
+        {
+          echo "<tr>";
+          echo "<td>" . $i . "</td>";
+          echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
+          echo "<td>" . ($i > 1 ? number_format($simulation->monthlyContribution, 2, ",", ".") : "---") . "</td>";
+          echo "<td>" . number_format($simulation->initialContribution * $simulation->monthlyIncome / 100, 2, ",", ".") . "</td>";
+          $simulation->initialContribution = $simulation->initialContribution * (1 + ($simulation->monthlyIncome / 100));
+          echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
+          $simulation->initialContribution += $simulation->monthlyContribution;
+          echo "</tr>";
+        }
+        echo "    </tbody>
+                        </table>";
+      }
+    }
+    R::close();
+
+    ?>
+
+    </fieldset>
+
+    <br>
+
+    <a href="index.html"><button>Back</button></a>
+  </main>
+
+  <footer>
+    <p>Edson Júnior & Wanderson Rodrigues - &copy; 2023</p>
+  </footer>
 </body>
 
 </html>
--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -16,14 +16,30 @@
 
     <main>
         <?php
-            if(isset($_GET['client']) && $_GET['client'] != "" && 
-             isset($_GET['initialContribution']) && 
-             isset($_GET['period']) && $_GET['period'] > 1 &&
-             isset($_GET['monthlyincome']) &&  
-             isset($_GET['monthlycontribution'])){
-                require_once './classes/autoloader.class.php';
-                
-                R::setup('mysql:host=localhost;dbname=fintech', 'root', '');
+        session_start();
+
+        if (
+            isset($_GET['client']) && $_GET['client'] != "" &&
+            isset($_GET['initialContribution']) &&
+            isset($_GET['period']) && $_GET['period'] > 0 &&
+            isset($_GET['monthlyincome']) &&
+            isset($_GET['monthlycontribution'])
+        ) 
+        {
+            require_once './classes/autoloader.class.php';
+
+            R::setup('mysql:host=localhost;dbname=fintech', 'root', '');
+
+            $inputIdentifier = md5(serialize($_GET));
+
+            if (!isset($_SESSION['input_identifiers'])) 
+            {
+                $_SESSION['input_identifiers'] = array();
+            }
+
+            if (!in_array($inputIdentifier, $_SESSION['input_identifiers'])) 
+            {
+                $_SESSION['input_identifiers'][] = $inputIdentifier;
 
                 $s = R::dispense('simulation');
 
@@ -36,55 +52,65 @@
                 $id = R::store($s);
 
                 $simulation = R::load('simulation', $id);
-                    echo "<ul>";
+                echo "<ul>";
+                echo "<h3>Data:</h3>";
+                echo "<li>ID: {$simulation->id}</li>";
+                echo "<li>Client: {$simulation->client}</li>";
+                echo "<li>Initial contribution: R$ " . number_format($simulation->initialContribution, 2, ',', '.') . "</li>";
+                echo "<li>Period: {$simulation->period} months</li>";
+                echo "<li>Monthly income: {$simulation->monthlyIncome} %</li>";
+                echo "<li>Monthly contribution: " . number_format($simulation->monthlyContribution, 2, ',', '.') . "</li>";
 
-                    echo "<li>ID: {$simulation->id}</li>";
-                    echo "<li>Cliente: {$simulation->client}</li>";
-                    echo "<li>Contribuição Inicial R$ ". number_format($simulation->initialContribution, 2, ',' , '.') . "</li>";
-                    echo "<li>Período: {$simulation->period} meses</li>";
-                    echo "<li>Taxa mensal: {$simulation->monthlyIncome} %</li>";
-                    echo "<li>Contribuição mensal: " . number_format($simulation->monthlyContribution, 2, ',' , '.') . "</li>";
+                echo "</ul>";
 
-                    echo "</ul>";
+                echo "
+            <table>
+                <thead>
+                    <tr>
+                        <th>Month</th>
+                        <th>Initial value (R$)</th>
+                        <th>Contribution (R$)</th>
+                        <th>Income (R$)</th>
+                        <th>Total (R$)</th>
+                    </tr>
+                </thead>
+                <tbody>";
 
-                    echo "<fieldset>
-                            <legend>Result</legend>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th>Mês</th>
-                                            <th>Valor Inicial (R$)</th>
-                                            <th>Aporte (R$)</th>
-                                            <th>Rendimento (R$)</th>
-                                            <th>Total (R$)</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>";
-                                        
-                                        for ($i = 1; $i <= $simulation->period; $i++) {
-                                            echo "<tr>";
-                                            echo "<td>" . $i . "</td>";
-                                            echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
-                                            echo "<td>" . ($i > 1 ? number_format($simulation->monthlyContribution, 2, ",", ".") : "---") . "</td>";
-                                            echo "<td>" . number_format($simulation->initialContribution * $simulation->monthlyIncome / 100, 2, ",", ".") . "</td>";
-                                            $simulation->initialContribution = $simulation->initialContribution * (1 + ($simulation->monthlyIncome / 100));
-                                            echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
-                                            $simulation->initialContribution += $simulation->monthlyContribution;
-                                            echo "</tr>";
-                                        }
-                    echo "          </tbody>
-                                </table>
-                        </fieldset>";
-                R::close();
-            }else{
-                echo "<h2>Valores não definidos!</h2>";
+                for ($i = 1; $i <= $simulation->period; $i++) 
+                {
+                    echo "<tr>";
+                    echo "<td>" . $i . "</td>";
+                    echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
+                    echo "<td>" . ($i > 1 ? number_format($simulation->monthlyContribution, 2, ",", ".") : "---") . "</td>";
+                    echo "<td>" . number_format($simulation->initialContribution * $simulation->monthlyIncome / 100, 2, ",", ".") . "</td>";
+                    $simulation->initialContribution = $simulation->initialContribution * (1 + ($simulation->monthlyIncome / 100));
+                    echo "<td>" . number_format($simulation->initialContribution, 2, ",", ".") . "</td>";
+                    $simulation->initialContribution += $simulation->monthlyContribution;
+                    echo "</tr>";
+                }
+                echo "</tbody>
+            </table>";
+            } 
+            else 
+            {
+                echo "<p>Data with the same input values ​​has already been sent.<br>
+                If you want to see the results go to the <a href=\"./historic.php\">history page</a>.</p>";
             }
+
+            R::close();
+        } 
+        else 
+        {
+            echo "<p>Values not defined!</p>";
+        }
         ?>
+
+        <br>
+        <a href="input.html"><button>re-simulate</button></a>
     </main>
-            <br><br>
-            <a href="/input.html"><button>re-simulate</button></a>
+
     <footer>
-        <p>WanRod e Edson Júnior- &copy; 2023</p>
+        <p>Edson Júnior & Wanderson Rodrigues - &copy; 2023</p>
     </footer>
 </body>
 
